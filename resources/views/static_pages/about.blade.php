975eab2cbd2fd625d4d70b4d7727d6a7db2dc0ff
e <1521973280@qq.com>
2018-08-08 19:22:21 +0800

    first-project

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sample App</title>
+</head>
+<body>
+  <h1>关于页</h1>
+  <h1>关于页</h1>
+  <h1>关于页</h1>
+  <h1>关于页</h1>
+  <h1>关于页</h1>
+  <h1>关于页</h1>
+  
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('static_pages/home');
+});
+
+Route::get('/home', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
